6a1d1bbb26ccdb771d09178bbc542a34c1a09dde
UunizumehValentin <valentineuunizumehnuobetah77@gmail.com>
2021-03-29 10:16:04 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>TRACKER ONE</h1>
+        <form id="to-do-form">
+          <input type="text" placeholder="enter text" />
+          <button type="submit">Add</button>
+        </form>
       </header>
     </div>
   );
 }
-
 export default App;
